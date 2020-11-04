693b2a7f7b983198ccae3b9f4c912e7b9a58a69a
Humphrey Murray <humphrey@datalive.com.au>
2020-11-04 12:44:44 +1100

    Updating settings to fallback to using the standard django settings approach if env.ini is not specified, since this is how most other reusable apps handle their settings.

--- instagram_profile/settings.py
+++ instagram_profile/settings.py
@@ -1,14 +1,22 @@
 from configparser import RawConfigParser
 from django.conf import settings
+import os
 
 
-env = RawConfigParser()
-env.read(settings.BASE_DIR + '/env.ini')
+_env_path = settings.BASE_DIR + '/env.ini'
 
-INSTAGRAM_ACCOUNT = env['instagram']['account']
-INSTAGRAM_AUTH_URL = env['instagram']['auth_url']
-INSTAGRAM_ACCESS_TOKEN_URL = env['instagram']['access_token_url']
-INSTAGRAM_APP_ID = env['instagram']['app_id']
-INSTAGRAM_SECRET = env['instagram']['secret']
-INSTAGRAM_REDIRECT_URL = env['instagram']['redirect_url']
-INSTAGRAM_MEDIA_URL = env['instagram']['media_url']
+if os.path.exists(_env_path):
+    env = RawConfigParser()
+    env.read(_env_path)
+    _settings = env['instagram']
+else:
+    _settings = settings.INSTAGRAM_PROFILE
+
+
+INSTAGRAM_ACCOUNT = _settings['account']
+INSTAGRAM_AUTH_URL = _settings['auth_url']
+INSTAGRAM_ACCESS_TOKEN_URL = _settings['access_token_url']
+INSTAGRAM_APP_ID = _settings['app_id']
+INSTAGRAM_SECRET = _settings['secret']
+INSTAGRAM_REDIRECT_URL = _settings['redirect_url']
+INSTAGRAM_MEDIA_URL = _settings['media_url']
