cffba589db2b76c6ea3d881923eba1418eae9d02
Humphrey Murray <humphrey@datalive.com.au>
2020-11-05 14:14:21 +1100

    apps.py verbose_name & and wire up default config

---
+++ instagram_profile/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'instagram_profile.apps.InstagramConfig'
--- instagram_profile/apps.py
+++ instagram_profile/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class InstagramConfig(AppConfig):
     name = 'instagram_profile'
-    verbose_name = 'Instagram profile'
+    verbose_name = 'Instagram sync'
