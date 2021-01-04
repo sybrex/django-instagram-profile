5ab9d1b1aad74776a10377229415e797906ca9eb
Viktor Dziuban <vsybrex@gmail.com>
2021-01-04 16:42:53 +0200

    Refactoring the settings

--- instagram_profile/settings.py
+++ instagram_profile/settings.py
@@ -1,24 +1,12 @@
-from configparser import RawConfigParser
 from django.conf import settings
-import os
 
 
-_env_path = settings.BASE_DIR + '/env.ini'
-
-if os.path.exists(_env_path):
-    env = RawConfigParser()
-    env.read(_env_path)
-    _settings = env['instagram']
-else:
-    _settings = settings.INSTAGRAM_PROFILE
-
-
-INSTAGRAM_ACCOUNT = _settings.get('account', None)
-INSTAGRAM_AUTH_URL = _settings.get('auth_url', 'https://api.instagram.com/oauth/authorize')
-INSTAGRAM_ACCESS_TOKEN_URL = _settings.get('access_token_url', 'https://api.instagram.com/oauth/access_token')
-INSTAGRAM_LONG_LIVED_ACCESS_TOKEN_URL = _settings.get('long_lived_access_token_url', 'https://graph.instagram.com/access_token')
-INSTAGRAM_REFRESH_ACCESS_TOKEN_URL = _settings.get('refresh_access_token_url', 'https://graph.instagram.com/refresh_access_token')
-INSTAGRAM_APP_ID = _settings['app_id']
-INSTAGRAM_SECRET = _settings['secret']
-INSTAGRAM_REDIRECT_URL = _settings['redirect_url']
-INSTAGRAM_MEDIA_URL = _settings.get('media_url', 'https://graph.instagram.com')
+INSTAGRAM_AUTH_URL = 'https://api.instagram.com/oauth/authorize'
+INSTAGRAM_ACCESS_TOKEN_URL = 'https://api.instagram.com/oauth/access_token'
+INSTAGRAM_LONG_LIVED_ACCESS_TOKEN_URL = 'https://graph.instagram.com/access_token'
+INSTAGRAM_REFRESH_ACCESS_TOKEN_URL = 'https://graph.instagram.com/refresh_access_token'
+INSTAGRAM_MEDIA_URL = 'https://graph.instagram.com'
+INSTAGRAM_ACCOUNT = None
+INSTAGRAM_APP_ID = settings.INSTAGRAM_PROFILE['app_id']
+INSTAGRAM_SECRET = settings.INSTAGRAM_PROFILE['secret']
+INSTAGRAM_REDIRECT_URL = settings.INSTAGRAM_PROFILE['redirect_url']
